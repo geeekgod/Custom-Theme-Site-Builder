16f7f89a9d57a4e0bd6cff55cd459d97d4719590
Rishabh Singh <ris8645208@gmail.com>
2022-06-08 16:00:54 +0530

    Revert "chore: add basic theme with context"
    This reverts commit fc2de45a1aebbb1cc986cb6be293224c65d4b87c.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { ThemeContext } from './Context/ThemeContext';
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-const App = () => {
-
-  const { theme, selectedTheme, setSelectedTheme } = useContext(ThemeContext)
-
+function App() {
   return (
-    <div style={{ background: theme[selectedTheme].bgPrimary, height: "100vh" }}>
-      <h1 style={{ color: theme[selectedTheme].primary }}>Hello</h1>
-      <button onClick={() => setSelectedTheme(0)}>First</button>
-      <button onClick={() => setSelectedTheme(1)}>Second</button>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { ThemeContextProvider } from './Context/ThemeContext';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ThemeContextProvider>
-      <App />
-    </ThemeContextProvider>
+    <App />
   </React.StrictMode>
 );
 
