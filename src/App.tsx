9ebaeeaf72e24d2d132e6a87318463569ab0dacf
Rishabh Singh <ris8645208@gmail.com>
2022-06-08 16:32:25 +0530

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext } from 'react';
 import { ThemeContext } from './Context/ThemeContext';
 
 const App = () => {
