fc2de45a1aebbb1cc986cb6be293224c65d4b87c
Rishabh Singh <ris8645208@gmail.com>
2022-06-08 15:53:33 +0530

    chore: add basic theme with context

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useContext, useEffect, useState } from 'react';
+import { ThemeContext } from './Context/ThemeContext';
 
-function App() {
+const App = () => {
+
+  const { theme, selectedTheme, setSelectedTheme } = useContext(ThemeContext)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ background: theme[selectedTheme].bgPrimary, height: "100vh" }}>
+      <h1 style={{ color: theme[selectedTheme].primary }}>Hello</h1>
+      <button onClick={() => setSelectedTheme(0)}>First</button>
+      <button onClick={() => setSelectedTheme(1)}>Second</button>
     </div>
   );
 }
