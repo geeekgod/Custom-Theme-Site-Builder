bb5ad1f2caade245491a7596941b33b3d552a7d5
shreyaa <shetshreyaa25@gmail.com>
2022-10-04 12:32:40 +0530

    changes

--- src/Components/Display/index.tsx
+++ src/Components/Display/index.tsx
@@ -27,6 +27,36 @@ const Display = ({ selectedTheme, theme }: displayProps) => {
         aliquid quod? Ipsam facilis temporibus sint quia optio voluptatum error,
         consequuntur a nobis veritatis fuga, magnam voluptates saepe asperiores.
       </p>
+      <p
+        style={{
+          color: theme[selectedTheme].secondary,
+          fontSize: theme[selectedTheme].fontSize2,
+        }}
+      >
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id eum cum
+        aliquid quod? Ipsam facilis temporibus sint quia optio voluptatum error,
+        consequuntur a nobis veritatis fuga, magnam voluptates saepe asperiores.
+      </p>
+      <p
+        style={{
+          color: theme[selectedTheme].secondary,
+          fontSize: theme[selectedTheme].fontSize2,
+        }}
+      >
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id eum cum
+        aliquid quod? Ipsam facilis temporibus sint quia optio voluptatum error,
+        consequuntur a nobis veritatis fuga, magnam voluptates saepe asperiores.
+      </p>
+      <p
+        style={{
+          color: theme[selectedTheme].secondary,
+          fontSize: theme[selectedTheme].fontSize2,
+        }}
+      >
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id eum cum
+        aliquid quod? Ipsam facilis temporibus sint quia optio voluptatum error,
+        consequuntur a nobis veritatis fuga, magnam voluptates saepe asperiores.
+      </p>
     </div>
   );
 };
